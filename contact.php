854cf7452142337e42c2cc10e6f37c5f4b1aabfd
juthamaswi-dot <juthamas.wi@rmutsvmail.com>
2026-02-20 16:19:16 +0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -15,7 +15,7 @@
             <td width="190" bgcolor="#FFFF66"><ul>
 				<li><a href="home.php">หน้าแรกกกกกกกกกกกกกกก</li>
 				<li><a href="room.php">ห้องพัก&ราคา</li>
-				<li><a herf="guest.php">สมุดเยี่ยม</li>
+				<li><a herf="guest.php">สมุดเยี่ยมมมมมมมมมมมมมมมม</li>
 				<li><a href="contact.php">ติดต่ออออออออออออออ</li>
 				</ul><p>
 				<?php include "login_form.php";?>
